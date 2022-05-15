f9a402794c4b08d7e5c21d88c6c57bc757da83ed
yakuthun <yakuthun@gmail.com>
2022-05-16 01:32:42 +0300

    ui katmani bitti

---
+++ oop-core-n-arch/Controllers/CustomerController.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace oop_core_n_arch.Controllers
+{
+    public class CustomerController : Controller
+    {
+        CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
+        public IActionResult Index()
+        {
+            var values = customerManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddCustomer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCustomer(Customer p)
+        {
+            customerManager.TInsert(p);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteCustomer(int id)
+        {
+            var value = customerManager.GetById(id);
+            customerManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateCustomer(int id)
+        {
+            var value = customerManager.GetById(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateCustomer(Customer p)
+        {
+            customerManager.TUpdate(p);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ oop-core-n-arch/Controllers/ProductController.cs
@@ -0,0 +1,66 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.FluentValidation;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace oop_core_n_arch.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+        public IActionResult Index()
+        {
+            var values = productManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            ProductValidator validationRules = new ProductValidator();
+            ValidationResult results = validationRules.Validate(p);
+            if (results.IsValid)
+            {
+                productManager.TInsert(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+        public IActionResult DeleteProduct(int id)
+        {
+            var value = productManager.GetById(id);
+            productManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            //güncelleem yapmasını istediğimiz alanı buldurduk.
+            var value = productManager.GetById(id); 
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateProduct(Product p)
+        {//bu alanda güncelleme işlemi yapılır.
+            productManager.TUpdate(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
